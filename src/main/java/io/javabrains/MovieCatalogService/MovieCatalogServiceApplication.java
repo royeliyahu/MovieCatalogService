557eade5b72163272ad43a20aa59a2b01c037742
Roy Eliyahu <RoyE@gmul-barak.co.il>
2019-07-01 12:40:31 +0300

    Initial commit

---
+++ src/main/java/io/javabrains/MovieCatalogService/MovieCatalogServiceApplication.java
@@ -0,0 +1,16 @@
+package io.javabrains.MovieCatalogService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+public class MovieCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogServiceApplication.class, args);
+	}
+
+}

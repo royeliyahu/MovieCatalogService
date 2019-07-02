4409d27f46e137895115392ce173c9765f9fa83a
RoyE <royeliyahu827@gmail.com>
2019-07-02 16:06:27 +0300

    catalog and userId

---
+++ src/main/java/io/javabrains/MovieCatalogService/MovieCatalogResource.java
@@ -0,0 +1,20 @@
+package io.javabrains.MovieCatalogService;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable String userId){
+        return Collections.singletonList(
+                new CatalogItem("Transformers", "test", 4)
+        );
+    }
+}
